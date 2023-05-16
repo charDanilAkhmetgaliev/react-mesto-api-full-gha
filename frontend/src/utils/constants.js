7be0afbf68898749e59fe9231c18cd92526acd2c
Danil <104395417+charDanilAkhmetgaliev@users.noreply.github.com>
2023-05-16 08:27:42 +0300

    Delete frontend/src directory

--- frontend/src/utils/constants.js
+++
@@ -1,9 +0,0 @@
-// данные подключения к серверу
-export const url = 'http://127.0.0.1:3000';
-// данные пользователя
-export const userAuthData = {
-  userToken: '320b4a75-5470-48e7-b4e8-36be65b57c43',
-  cohortName: 'cohort-58'
-}
-
-export const authUrl = 'http://127.0.0.1:3000';
